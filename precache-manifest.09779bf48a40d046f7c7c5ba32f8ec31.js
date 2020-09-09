d949424f1724e75321f78572b4005ced6c46ffa1
Salvatore <salvatore@Claudias-MacBook-Pro.local>
2020-09-09 11:49:54 +0200

    Updates

---
+++ precache-manifest.09779bf48a40d046f7c7c5ba32f8ec31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c828153ff16a98af0f9a8dcd48afc1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43e9daad532025c8be97",
+    "url": "/static/css/main.5af07758.chunk.css"
+  },
+  {
+    "revision": "c70ca3c15703a0cc6616",
+    "url": "/static/js/2.049586ef.chunk.js"
+  },
+  {
+    "revision": "d228efb7b414200ae80517a37394613a",
+    "url": "/static/js/2.049586ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e9daad532025c8be97",
+    "url": "/static/js/main.6865e8bc.chunk.js"
+  },
+  {
+    "revision": "8432007fabc4961e875c",
+    "url": "/static/js/runtime-main.73a895b7.js"
+  },
+  {
+    "revision": "9a76e74e107e6163dc82116b8236c132",
+    "url": "/static/media/login.9a76e74e.svg"
+  }
+]);
